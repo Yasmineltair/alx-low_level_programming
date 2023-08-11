3f3c2363de64fb02470f135cffb25fdf5547d525
Yasmineltair <yasmineltair293@gmail.com>
2023-08-11 15:22:00 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+* Return: always 0 (success)
+*/
+int main(void)
+{
+int n = 122;
+while (n >= 97)
+{
+	putchar(n);
+	n--;
+}
+putchar('\n');
+return (0);
+}
