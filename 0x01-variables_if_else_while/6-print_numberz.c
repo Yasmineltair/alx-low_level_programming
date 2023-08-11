54ded9be78fa3fe8128049bc4bd19a0974d1dbf4
Yasmineltair <yasmineltair293@gmail.com>
2023-08-11 15:11:20 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - entry point
+* Return: always 0 (success)
+*/
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+	putchar(n + 48);
+}
+putchar('\n');
+return (0);
+}
