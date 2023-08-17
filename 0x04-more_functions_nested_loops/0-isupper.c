138e101eb318dbfedbbd1f37f771a8570bc8a093
Yasmineltair <yasmineltair293@gmail.com>
2023-08-17 05:27:05 -0700

    0-isuper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
   * _isupper - checks for uppercase character
@@ -11,6 +10,6 @@ int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
 	return (1);
-else if (c >= 97 && c <= 122)
+else
 	return (0);
 }
