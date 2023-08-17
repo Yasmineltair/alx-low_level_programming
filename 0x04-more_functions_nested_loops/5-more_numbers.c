e0ad58b76e294c592714f9cb7b419f08be25636a
Yasmineltair <yasmineltair293@gmail.com>
2023-08-17 10:19:58 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,15 @@
 void more_numbers(void)
 {
 	int i;
-	char c;
+	int c;
 for (i = 1; i <= 10; i++)
 {
-for (c = '0'; c <= '14'; c++)
+for (c = 0; c <= 14; c++)
 {
 if (c >= 10)
 {
-	_putchar('1');
-	_putchar(c % 10 + '0');
+	_putchar(1);
+	_putchar(c % 10 + 0);
 }
 _putchar(c);
 	}
