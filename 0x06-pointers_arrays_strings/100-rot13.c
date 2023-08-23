1ea8cc5042858431c9125672751f4b2cdbe0ea61
Yasmineltair <yasmineltair293@gmail.com>
2023-08-23 16:18:26 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * rot13 - function that encodes a string using rot13.
+  * @c: string
+  * Return: char c
+  *
+  */
+
+char *rot13(char *c)
+{
+int i;
+char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+char *ptr = c;
+while (*c)
+{
+	for (i = 0; i <= 52; i++)
+	{
+		if (*c == rot13[i])
+		{
+			*c = ROT13[i];
+			break;
+		}
+	}
+	c++;
+}
+return (ptr);
+}
