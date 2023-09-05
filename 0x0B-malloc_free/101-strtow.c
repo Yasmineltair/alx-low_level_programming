58a141ad6af65830d886103d36392e0613443602
Yasmineltair <yasmineltair293@gmail.com>
2023-09-05 12:05:07 -0700

    1

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * wrdcount - function for word count
+  * @s: input string
+  * Return: number of string
+  */
+
+int wrdcount(char *s);
+{
+int i = 0;
+int n = 0;
+for (; s[i]; i++)
+	if (s[i] == " ")
+	{
+		if (s[i + 1] != " " && s[i + 1] != '\0')
+			n++;
+		else if (i == 0)
+			n++;
+	}
+n++;
+return (n);
+}
