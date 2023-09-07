f34c66f8acd3f3b46e0483c2bfb90bceccfa0b96
Yasmineltair <yasmineltair293@gmail.com>
2023-09-07 11:02:42 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * _memset - function to set value to array
+  * @b: replaces value
+  * @n: number of bytes
+  * Return: pointer
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+char *ptr = s;
+while (n--)
+	*s++ = b;
+return (ptr);
+}
+
+/**
+  * _calloc - allocates memory for an array
+  * @nmemb: no of array elements
+  * @size: array size
+  * Return: pointer to the allocated memory.
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *a;
+if (size == 0 || nmemb == 0)
+	return (NULL);
+a = malloc(sizeof(int) * nmemb);
+if (a == 0)
+	return (NULL);
+_memset(a, 0, sizeof(int) * nmemb);
+return (a);
+}
