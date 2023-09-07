f9f9945f8d24adf03ff28b2fe16fad8e9be10637
Yasmineltair <yasmineltair293@gmail.com>
2023-09-07 16:45:56 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ int _strlen(char *s)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *c;
-int size1, size2;
+unsigned int size1, size2;
 unsigned int i, j;
 
 if (s1 == NULL)
@@ -35,6 +35,9 @@ if (s2 == NULL)
 	s2 = "";
 size1 = _strlen(s1);
 size2 = _strlen(s2);
+if (n < size2)
+	c = malloc(size1 + n + 1);
+else
 c = malloc(size1 + size2 + 1);
 if (c == NULL)
 {
@@ -42,7 +45,7 @@ if (c == NULL)
 }
 for (i = 0; s1[i] != '\0'; i++)
 c[i] = s1[i];
-for (j = 0; s2[j] != '\0' && j <= n; j++)
+for (j = 0; s2[j] != '\0' && j < n; j++)
 {
 c[i + j] = s2[j];
 }
