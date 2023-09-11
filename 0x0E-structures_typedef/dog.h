075b327778111dbdab1ec973850102f943bf58e7
Yasmineltair <yasmineltair293@gmail.com>
2023-09-11 11:33:31 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+/**
+  * struct dog - a dog struct
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: the dog's owner
+  */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+#endif
