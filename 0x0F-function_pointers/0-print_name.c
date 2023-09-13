4dcb4ee4ef11b85f0ba3baa37089b1501a408fe6
Yasmineltair <yasmineltair293@gmail.com>
2023-09-13 04:12:32 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+  * print_name -  function that prints a name.
+  * @name: input name
+  * @f: pointer to function
+  * Return: void
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
