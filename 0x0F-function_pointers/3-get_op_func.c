3b44cdc473aaf7657fcc0c8bddaa072afe044a71
Yasmineltair <yasmineltair293@gmail.com>
2023-09-13 16:06:35 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,13 +17,8 @@ op_t ops[] = {
 };
 int i = 0;
 
-while (i < 5)
-{
-if (s && s[0] == ops[i].op[0] && !s[1])
-{
+while (ops[i].op != NULL && *(ops[i].op != *s)
+		i++;
 	return (ops[i].f);
-	i++;
 }
-}
-return (NULL);
-}
+
