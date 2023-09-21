6a597c4a2cf73be28374e51dc880820388d39375
Yasmineltair <yasmineltair293@gmail.com>
2023-09-21 11:47:00 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
-  * _strlen - function to get the string length
-  * @str: input string
-  * Return: string length
-  */
-
-int _strlen(char *str)
-{
-	int i = 0;
-
-	if (!str)
-		return (0);
-	while (str++)
-		i++;
-	return (i);
-}
-
-/**
   * print_list - prints all elements of list
   * @h: pointer to the first node
   * Return: the number of nodes
@@ -29,10 +12,13 @@ size_t i = 0;
 
 while (h)
 {
-	printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
-	h = h->next;
-	i++;
+	if (!h->str)
+		printf("[0] (nil)\n");
+		else
+		
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			i++;
 }
 return (i);
 }
-
