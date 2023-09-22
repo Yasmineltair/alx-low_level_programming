035489c956670e5fe517e25c9a5e620c5aaabc04
Yasmineltair <yasmineltair293@gmail.com>
2023-09-21 18:29:53 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,13 +13,15 @@ size_t i = 0;
 while (h)
 {
 	if (!h->str)
+	{
 		printf("[0] (nil)\n");
+	}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			i++;
 		}
+		i++;
+		h = h->next;
 }
 return (i);
 }
