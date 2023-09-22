df0b6d0fdc87c20dc40d8f3a86cc3d27b0c31659
Yasmineltair <yasmineltair293@gmail.com>
2023-09-21 19:23:28 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+  * _constructor - function that prints before name
+  * Return: void
+  */
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
