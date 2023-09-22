831184567c9249fda6dd93d3d545acf163f0c156
Yasmineltair <yasmineltair293@gmail.com>
2023-09-21 19:01:27 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,19 @@
 #include "lists.h"
 
 /**
+  * _strlen - function to get string length
+  * @str: input string
+  * Return: length of string
+  */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+/**
   * add_node_end - function that adds a new node at the end
   * @head: pointer to the head node
   * @str: needs to be dublicated
@@ -9,38 +22,35 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *current_node = *head;
-	size_t i;
-list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node, *current_node;
 
-if (new_node == NULL || head == NULL)
-{
-	return (NULL);
-}
-if (str)
-{
-new_node->str = strdup(str);
-if (!new_node->str)
-{
-	free(new_node);
-	return (NULL);
-}
-for (i = 0; str[i]; i++)
-;
-new_node->len = i;
-}
-if (current_node)
-{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = _strlen(new_node->str);
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	current_node = *head;
 	while (current_node->next)
 	{
 		current_node = current_node->next;
-		current_node->next = new_node;
 	}
+	current_node->next = new_node;
+	return (new_node);
 }
-else
-{
-	*head = new_node;
-}
-return (new_node);
-}
-
