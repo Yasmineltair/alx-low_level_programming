c97939875d107e6f4d031a5e792ffbf2f2e49f27
Yasmineltair <yasmineltair293@gmail.com>
2023-10-02 14:50:35 -0700

    new

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 {
 int fd;
 ssize_t len = _strlen(text_content);
-ssize_t bytes;
+ssize_t bytes = 0;
 
 if (filename == NULL)
 	return (-1);
