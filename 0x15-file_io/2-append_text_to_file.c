6a0001ff8b25d4543dbd2881a548b65b8d8a06ad
Yasmineltair <yasmineltair293@gmail.com>
2023-10-02 14:32:35 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,6 +9,7 @@
 int _str_len(char *s)
 {
 	int i = 0;
+
 	if (!s)
 		return (0);
 	while (*s++)
@@ -29,13 +30,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t len = _str_len(text_content);
 	ssize_t bytes = 0;
 
-	if (!filename)
-		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return(-1);
-	if (len)
-		bytes = write(fd, text_content, len);
-	close(fd);
-	return (bytes == len ? 1 : -1);
+if (!filename)
+	return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+	return (-1);
+if (len)
+	bytes = write(fd, text_content, len);
+close(fd);
+return (bytes == len ? 1 : -1);
 }
