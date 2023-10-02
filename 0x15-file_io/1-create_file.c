68673ba3fc3617afcf28fbdb8409da05d3fe04d7
Yasmineltair <yasmineltair293@gmail.com>
2023-10-02 11:10:20 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ if (text_content != NULL)
 	for (len  = 0; text_content[len]; len++)
 		;
 }
-fd = open(filename, O_CREAT | O_RDWR | O_TRUNC | S_IRUSR | S_IWUSR);
+fd = open(filename, O_WRONLT | O_CREAT | O_TRUNC , 0600);
 w = write(fd, text_content, len);
 	if (fd == -1 || w == -1)
 	return (-1);
