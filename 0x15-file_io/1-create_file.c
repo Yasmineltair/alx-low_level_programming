30278f3309f50ed7c6bee550a3773e9018b5f9e4
Yasmineltair <yasmineltair293@gmail.com>
2023-10-02 14:35:32 -0700

    new

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,8 +10,8 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-int len;
-int w;
+ssize_t len;
+ssize_t w;
 
 if (filename == NULL)
 	return (-1);
@@ -22,7 +22,7 @@ if (text_content != NULL)
 }
 fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 w = write(fd, text_content, len);
-	if (fd == -1 || w == -1)
+	if (fd == -1)
 	return (-1);
 	close(fd);
 	return (1);
