ad8be2d4831f073f9b7adc3804e4e3050241588e
Yasmineltair <yasmineltair293@gmail.com>
2023-12-01 13:59:55 -0800

    up

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,7 +5,7 @@
   * @ht: the hash table you want to add or update the key/value to
   * @key: the key
   * @value: the value associated with the key
-  * Retuen: 1 if it succeeded, 0 otherwise
+  * Return: 1 if it succeeded, 0 otherwise
   */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -37,9 +37,54 @@ return (1);
 }
 
 /**
-  * updt_node - 
-  *
-  *
-  *
+  * updt_node - update node
+  * @node: pointer to the existed node
+  * @key: key
+  * @value: value of the updated or new node
+  * Return: pointer to the head
   */
+
+hash_node_t *updt_node(hash_node_t *node, const char *key, char *value)
+{
+	hash_node_t *temp = node;
+
+	while (temp)
+	{
+		if (!strcmp(key, temp->key))
+		{
+			free(temp->value);
+			temp->value = value;
+			return (node);
+		}
+		temp = temp->next;
+	}
+	temp = node;
+	node = new_nod(node);
+	if (!node)
+	{
+		node = temp;
+		return (node);
+	}
+	node->key = strdup(key);
+	node->value = value;
+	return (node);
+}
+
+/**
+  * new_nod - make a new node
+  * @e_node: exist node
+  * Return: pointer to the new node
+  */
+
+hash_node_t *new_nod(hash_node_t *e_node)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+	node->key = NULL;
+	node->value = NULL;
+	node->next = e_node;
+	return (node);
 }
