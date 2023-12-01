49a22ce98f9d1e03424b8a477d321494c3c7dfdd
Yasmineltair <yasmineltair293@gmail.com>
2023-12-01 14:56:24 -0800

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_print -  prints a hash table.
+  * @ht: hash table
+  */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index, i;
+	hash_node_t *node;
+
+	index = i = 0;
+	if (!ht)
+	return;
+	printf("{");
+	while (index < ht->size)
+	{
+		node = ht->array[index];
+		while (node)
+		{
+			printf("\'%s\':\'%s\'", node->key, node->value);
+			if (node->next)
+				printf(", ");
+			node = node->next;
+		}
+		while (!(ht->array[index + 1]))
+			index++;
+		if (i && index < ht->size)
+			printf(", ");
+		else
+			i = 1;
+		index++;
+	}
+	printf("}\n");
+}
